95a62c7018871a5cd1cd07c09f66150023080d03
Marta Kozak <ma.maaravi@gmail.com>
2019-09-11 12:27:15 +0300

    started cleaning the code

--- main.py
+++ main.py
@@ -3,17 +3,35 @@ import tkinter.filedialog
 import tkinter.messagebox
 import tesseract_core
 import interface_settings
+var = 0
 
 def file_save():
-    fout = tk.filedialog.asksaveasfile(mode='w', defaultextension=".txt")
-    fout.write(tesseract_core.str_tess)
-    fout.close()
-    #message about success
+    global var
+    if var.get() == 0:
+        extension = '.pdf'
+    elif var.get() == 1:
+        extension = '.doc'
+    elif var.get() == 2:
+        extension = '.txt'
+    try:
+        fout = tk.filedialog.asksaveasfile(mode='w', defaultextension=extension)
+        fout.write(tesseract_core.str_tess)
+        fout.close()
+        tk.messagebox.showinfo("Img OCR", "Successfully saved!")
+    except AttributeError:
+        tk.messagebox.showerror("No path chosen!", "Please, choose the path.")
+
+
+
+
+
 def main():
 
     window_upload_stage = tk.Tk()
 
     interface_settings.create_window(window_upload_stage)
+    global add_img
+    add_img=tk.PhotoImage(file=r"D:\Python Projects\OCR_Project\plus.png")
 
 
     #adding text instruction on a window_upload_stage
@@ -24,25 +42,30 @@ def main():
 
     f_path = ''
     def browse_a_file():
-        f_path=tk.filedialog.askopenfilename(initialdir="/", title="Select an Image", filetype=(("JPG File", "*.jpg"), ("All files", "*.*")))
-        if not f_path:
+        f_path=tk.filedialog.askopenfilename(initialdir="/", title="Select an Image", filetype=(("PNG File", "*.png"), ("All files", "*.*")))
+        # if not f_path:
+        #     tk.messagebox.showerror("No Image chosen!", "Please, choose the image.")
+        # else:
+        #     someFunc(f_path)
+        try:
+            f_path=tk.filedialog.askopenfilename(initialdir="/", title="Select an Image", filetype=(("PNG File", "*.png"), ("All files", "*.*")))
+        except AttributeError:
             tk.messagebox.showerror("No Image chosen!", "Please, choose the image.")
             exit()
-        else:
-            someFunc(f_path)
+           
 
 
 
     
 
-    add_img=tk.PhotoImage(file=r"D:\Python Projects\OCR_Project\plus.png")
 
-    btn_upload=tk.Button(window_upload_stage, text="Add Image", command=browse_a_file)
+    btn_upload=tk.Button(window_upload_stage, image=add_img, command=browse_a_file)
     btn_upload.config(anchor=tk.CENTER)
     btn_upload.pack()
 
     
     def someFunc(f_path):
+        global var
         window_upload_stage.destroy()
         tesseract_core.image_ocr(f_path)
         window_output_text = tk.Tk()
@@ -55,14 +78,21 @@ def main():
         text_ocr.place(x=0, y=250)
         text_ocr.configure(state="disabled")
         text_ocr.configure(inactiveselectbackground=text_ocr.cget("selectbackground"))
+
         button = tk.Button(window_output_text,
-                        text="Save", width =5, height = 5, command=file_save) #callback = SOME_FUNC_TO_SAVE_FILE_MB_WINDOW_TO_CHOOSE_EXTENSION
+                        text="Save", width =5, height = 5, command=file_save)
         button1 = tk.Button(window_output_text,
-                        text="Repeat", width =5, height = 5, command=restart) #callback = START_PROGRAM_AGAIN
+                        text="Repeat", width =5, height = 5, command=restart) 
         button.place(x=200, y=50)
         button1.place(x=300, y=50)
-
-
+        var = tk.IntVar()
+        var.set(0)
+        radiobutton1 = tk.Radiobutton(window_output_text, text  = 'PDF',  variable=var, value=0)
+        radiobutton2 = tk.Radiobutton(window_output_text, text='DOC', variable=var, value=1)
+        radiobutton3 = tk.Radiobutton(window_output_text, text='TXT', variable=var, value=2)
+        radiobutton1.place(x=500, y=50)
+        radiobutton2.place(x=500,y=70)
+        radiobutton3.place(x=500,y=90)
 
 
 
@@ -71,9 +101,7 @@ main()
 tk.mainloop()
 
 
-# передбачити помилки (трай кеч + меседжбокси) 
-# --- вибраний файл неправильного формату
-# 
+
 # зробити .exe
 # гарно і логічно розкидати все по файлах
 # додати змістовні коментарі
